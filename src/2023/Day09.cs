06cd65450c44b39cb7944c0acb6c099e1fa1a2c0
Ingemar Carlsson <ICARLSS6@volvocars.com>
2023-12-10 15:12:45 +0100

    to long

--- src/2023/Day09.cs
+++ src/2023/Day09.cs
@@ -1,23 +1,23 @@
 public class Day09(string[] input)
 {
-    public int RunA()
+    public long RunA()
     {
-        var parsed = input.Select(x => x.Split(" ").Select(x => int.Parse(x)).ToList()).ToList();
+        var parsed = input.Select(x => x.Split(" ").Select(x => long.Parse(x)).ToList()).ToList();
 
-        int predictionsSum = 0;
+        long predictionsSum = 0;
         foreach (var line in parsed)
         {
-            List<List<int>> prediction = [];
-            prediction.Add(new List<int>(line));
+            List<List<long>> prediction = [];
+            prediction.Add(new List<long>(line));
             while (prediction.Last().Any(x => x != 0))
             {
                 var lastLine = prediction.Last();
-                var newLine = lastLine.Zip(lastLine.Skip(1), (a, b) => Math.Abs(a - b)).ToList();
+                var newLine = lastLine.Zip(lastLine.Skip(1), (a, b) => (b - a)).ToList();
                 prediction.Add(newLine);
             }
 
             prediction.Last().Add(0);
-            int newNumber = 0;
+            long newNumber = 0;
             for (int i = prediction.Count - 2; i >= 0; i--)
             {
                 newNumber = prediction[i + 1].Last() + prediction[i].Last();
@@ -28,7 +28,7 @@ public class Day09(string[] input)
         return predictionsSum;
     }
 
-    public int RunB()
+    public long RunB()
     {
         return 1;
     }
