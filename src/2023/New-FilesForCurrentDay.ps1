0a418c70960308b4afc4901722f4e40b3bf0d796
Ingemar Carlsson <ICARLSS6@volvocars.com>
2023-12-11 20:03:03 +0100

    helpers

--- src/2023/New-FilesForCurrentDay.ps1
+++ src/2023/New-FilesForCurrentDay.ps1
@@ -1,33 +1,24 @@
+[CmdletBinding()]
 param (
     [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
     [string]$FullName,
-    [string]$OutputPath
+    [string]$OutputPath = '.'
 )
 
 process {
     # Get the current day number
     $dayNumber = Get-Date -UFormat "%d"
 
+    # Get the new file name
+    $newFileName = (Split-Path -Leaf $FullName) -replace "XY", $dayNumber 
+    
     # Read the content of the file
     $content = Get-Content -Path $FullName
-
+    
     # Replace "XY" with the day number in the content
     $content = $content -replace "XY", $dayNumber
+    
+    $newFileFullPath = Join-Path -Path $OutputPath -ChildPath $newFileName
 
-    # Write the updated content back to the file
-    Set-Content -Path $FullName -Value $content
-
-    # Rename the file
-    $newFileName = $FullName -replace "XY", $dayNumber
-    $newFileName = Split-Path -Leaf $newFileName  # Get the file name without the directory
-
-    if ($OutputPath) {
-        # If an output path is provided, use it
-        $newFileName = Join-Path -Path $OutputPath -ChildPath $newFileName
-    } else {
-        # Otherwise, use the same directory as the input file
-        $newFileName = Join-Path -Path (Split-Path -Parent $FullName) -ChildPath $newFileName
-    }
-
-    Rename-Item -Path $FullName -NewName $newFileName
+    Set-Content -Path $newFileFullPath -Value $content
 }
